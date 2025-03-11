0aad9c5e850e33523bd4579f4342c00638dc5cdb
Fergoes <49106825+fergoes14@users.noreply.github.com>
2025-03-11 01:15:29 -0300

    add img
    adicionado imagem aos projetos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,31 +7,33 @@ function Projects() {
       <h2>Meus Projetos</h2>
       <div className="project-container"> {/* Adicionado o container para os projetos */}
         <div className="project-card">
+
           <h3>Projeto 1</h3>
+          <div className="project-image-container">
+            <img src="https://i.imgur.com/fc7GzZX.png" alt="Projeto 1" className="project-image" /> {/* Imagem do projeto */}
+          </div>
           <p>Descrição do projeto</p>
           <a href="https://github.com/seu-usuario/projeto1" target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
         </div>
+
         <div className="project-card">
-          <h3>Projeto 2</h3>
+
+          <h3>Projeto 1</h3>
+          <div className="project-image-container">
+            <img src="https://i.imgur.com/Buz0LMR.png" alt="Projeto 1" className="project-image" /> {/* Imagem do projeto */}
+          </div>
           <p>Descrição do projeto</p>
-          <a href="https://github.com/seu-usuario/projeto2" target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
+          <a href="https://github.com/seu-usuario/projeto1" target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
         </div>
-        <div className="project-card">
-          <h3>Projeto 3</h3>
-          <p>Descrição do projeto</p>
-          <a href="https://github.com/seu-usuario/projeto3" target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
-        </div>
-        <div className="project-card">
-          <h3>Projeto 4</h3>
-          <p>Descrição do projeto</p>
-          <a href="https://github.com/seu-usuario/projeto4" target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
-        </div>
+
+        {/* Adicionar outros projetos aqui com a mesma estrutura */}
       </div>
     </section>
   );
 }
 
 export default Projects;
+
 
 
 
