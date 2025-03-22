1a7b12809cefcdfc9ef20b16edc84ee780cfd076
Fergoes <49106825+fergoes14@users.noreply.github.com>
2025-03-22 18:46:22 -0300

    projetos/contato
    projetos adicionados e msg de contato alterada

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,38 +3,42 @@ import { FaEnvelope, FaWhatsapp } from "react-icons/fa";
 import "../styles/Contact.css";
 
 function Contact() {
-    const handleWhatsAppClick = () => {
-        const message = "Olá, peguei seu contato no portfólio!";
-        const phoneNumber = "15998103788"; // Número de telefone no formato internacional sem espaços
-        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-        window.open(whatsappUrl, "_blank");
-    };
+  const handleWhatsAppClick = () => {
+    const message = "Olá, Fernando! Encontrei seu portfólio e gostaria de saber mais sobre seu trabalho como desenvolvedor. Podemos conversar?"
+      ;
+    const phoneNumber = "15998103788"; // Número de telefone no formato internacional sem espaços
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      message
+    )}`;
+    window.open(whatsappUrl, "_blank");
+  };
 
-    return (
-        <section id="contact" className="contact">
-            <h2>Contato</h2>
-            <p className="contact-description">
-                Você pode me contatar através dos canais abaixo. Fique à vontade para mandar uma mensagem, estou sempre disponível para conversar sobre projetos e outras oportunidades!
-            </p>
-            
-            <div className="contact-info">
-                <div className="contact-item">
-                    <FaEnvelope className="contact-icon" />
-                    <span className="contact-text">fergoes02@gmail.com</span>
-                </div>
+  return (
+    <section id="contact" className="contact">
+      <h2>Contato</h2>
+      <p className="contact-description">
+        Você pode me contatar através dos canais abaixo. Fique à vontade para
+        mandar uma mensagem, estou sempre disponível para conversar sobre
+        projetos e outras oportunidades!
+      </p>
 
-                <div className="contact-item">
-                    <FaWhatsapp className="contact-icon" />
-                    <span className="contact-text">(15) 99810-3788</span>
-                </div>
-            </div>
+      <div className="contact-info">
+        <div className="contact-item">
+          <FaEnvelope className="contact-icon" />
+          <span className="contact-text">fergoes02@gmail.com</span>
+        </div>
 
-            <button className="contact-button" onClick={handleWhatsAppClick}>
-                Entrar em Contato
-            </button>
-        </section>
-    );
+        <div className="contact-item">
+          <FaWhatsapp className="contact-icon" />
+          <span className="contact-text">(15) 99810-3788</span>
+        </div>
+      </div>
+
+      <button className="contact-button" onClick={handleWhatsAppClick}>
+        Entrar em Contato
+      </button>
+    </section>
+  );
 }
 
 export default Contact;
-
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,9 +6,31 @@ function Projects() {
     <section id="projects" className="projects">
       <h2>Meus Projetos</h2>
       <div className="project-container"> {/* Adicionado o container para os projetos */}
+
+        <div className="project-card">
+          <h3>Jump Pixel</h3>
+          <div className="project-image-container">
+            <img src="https://i.imgur.com/LQRitJP.png" alt="Projeto" className="project-image" /> {/* Imagem do projeto */}
+          </div>
+          <p>Game baseado no flappy-bird apenas para pc em React</p>
+          <a href="hhttps://github.com/fergoes14/jump-pixel" target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
+        </div>
+
+        <div className="project-card">
+          <h3>Nathy personalizados</h3>
+          <div className="project-image-container">
+            <img src="https://i.imgur.com/acPmuAn.png" alt="Projeto" className="project-image" /> {/* Imagem do projeto */}
+          </div>
+          <p>Vitrine em andamento para uma papelaria personalizada</p>
+
+          <a href="https://nathypersonalizados.netlify.app/" target="_blank" rel="noopener noreferrer">nathypersonalizados.netlify.app</a>
+        </div>
+
         <div className="project-card">
 
           <h3>Portfólio</h3>
+
+
           <div className="project-image-container">
             <img src="https://i.imgur.com/7PrgUml.png" alt="Projeto" className="project-image" /> {/* Imagem do projeto */}
           </div>
